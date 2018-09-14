c9cbd49cce2f5bfc9f1b3d048384abadf1a498a1
Charles Marttinen <mail@charlesmarttinen.ca>
2018-09-14 12:19:45 -0400

    Scroll active nav bar item into view on mobile
    On mobile, the navigation menu in the header
    scrolls horizontally if all the menu items don't
    fit on screen at once, but clicking a menu item
    that was partially off-screen didn't bring it
    fully into view. This commit fixes that.

--- src/components/LayoutHeader/HeaderLink.js
+++ src/components/LayoutHeader/HeaderLink.js
@@ -4,16 +4,33 @@ import classNames from 'classnames';
 
 import styles from './HeaderLink.module.scss';
 
-const HeaderLink = ({isActive, title, to}) => (
-    <Link
-        to={to}
-        className={classNames({
-            [styles.link]: true,
-            [styles.link_active]: isActive,
-        })}
-    >
-        {title}
-    </Link>
-);
-
-export default HeaderLink;
+export default class HeaderLink extends React.Component {
+    componentDidMount() {
+        if (this.props.isActive) {
+            this.scrollIntoView();
+        }
+    }
+    scrollIntoView = () => {
+        if (this.button.scrollIntoView) {
+            this.button.scrollIntoView({
+                behaviour: 'smooth',
+                inline: 'center',
+            });
+        }
+    };
+    render() {
+        const {isActive, title, to} = this.props;
+        return (
+            <Link
+                to={to}
+                className={classNames({
+                    [styles.link]: true,
+                    [styles.link_active]: isActive,
+                })}
+                innerRef={node => (this.button = node)}
+            >
+                {title}
+            </Link>
+        );
+    }
+}

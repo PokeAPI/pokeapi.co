b2f2d3345341be33dea72af4c473ba30da6788b5
Charles Marttinen <mail@charlesmarttinen.ca>
2018-09-21 17:16:44 -0400

    Disable service worker

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,6 @@ module.exports = {
                 icon: 'src/images/icon.png', // This path is relative to the root of the site.
             },
         },
-        'gatsby-plugin-offline',
+        // 'gatsby-plugin-offline',
     ],
 };

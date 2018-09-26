f90f0e3b320b2d7f8c6b3d0acd4a8b81803f2ed7
Charles Marttinen <mail@charlesmarttinen.ca>
2018-09-26 16:54:37 -0400

    refactor: Make TableOfContents a functional component
    Move the scroll event listener into a separate ActiveSection component.
    This component takes a render function and a list of objects that
    contain an `id` property as props, then calls the render function,
    passing in as an argument the object whose `id` matches the ID of the
    active page section.

--- src/pages/docs/v2.html.js
+++ src/pages/docs/v2.html.js
@@ -1,50 +1,48 @@
 import React, {Fragment} from 'react';
-// import {Link} from 'gatsby';
+import marked from 'marked';
 
 import Layout from '../../components/Layout';
 import DocsContainer from '../../components/DocsContainer';
 import DocSection from '../../components/DocSection';
-import TableOfContents from '../../components/TableOfContents';
 import makeId from '../../utils/makeId';
 
-import docs, {resourceLists, utility} from '../../docs';
+import docs from '../../docs';
 
-let toc = docs.map(doc => {
+let toc = docs.map((doc, i) => {
+    if (doc.separator) {
+        return {separator: true, id: String(i)};
+    }
     return {
         name: doc.name,
-        url: '#' + makeId(doc.name) + '-section',
+        id: makeId(doc.name) + '-section',
         children: doc.resources.map(resource => {
             return {
                 name: resource.name,
-                url: '#' + makeId(resource.name),
+                id: makeId(resource.name),
             };
         }),
     };
 });
 toc = [
-    {name: 'Information', url: '#info'},
-    {name: 'Fair Use Policy', url: '#fairuse'},
-    {name: 'Slack', url: '#slack'},
-    {name: 'Wrapper Libraries', url: '#wrap'},
-    {separator: true, name: '', url: ''},
-    {name: 'Resource Lists', url: '#resource-lists'},
-    {separator: true, name: '', url: 'y'},
+    {name: 'Information', id: 'info'},
+    {name: 'Fair Use Policy', id: 'fairuse'},
+    {name: 'Slack', id: 'slack'},
+    {name: 'Wrapper Libraries', id: 'wrap'},
+    {separator: true, id: 'x'},
     ...toc,
-    {separator: true, name: '', url: 'z'},
-    {name: 'Utility', url: '#utility-section'},
 ];
 
 export default ({location}) => (
     <Layout location={location}>
-        <DocsContainer TOC={() => <TableOfContents toc={toc} />}>
+        <DocsContainer toc={toc}>
             <h2 id="info">Information</h2>
             <p>
                 This is a <strong>consumption-only</strong> API—only the HTTP
                 GET method is available on resources.
             </p>
             <p>
-                No authentication is required to access this API, and all resources
-                are fully open and available. Since the move to static
+                No authentication is required to access this API, and all
+                resources are fully open and available. Since the move to static
                 hosting in autumn 2018, rate limiting has been changed to a
                 fixed limit of 100 API requests per IP address per minute.
             </p>
@@ -147,48 +145,33 @@ export default ({location}) => (
                 </li>
             </ul>
 
-            <h2 id="resource-lists">Resource Lists</h2>
-            <p>
-                Calling any API endpoint without a resource ID or name will
-                return a paginated list of available resources for that API. By
-                default, a list 'page' will contain up to 20 resources. If you
-                would like to change this just add a 'limit' query parameter,
-                e.g. <code>limit=60</code>.
-            </p>
-            {resourceLists.map(resourceList => (
-                <DocSection
-                    key={resourceList.name}
-                    rootUrl="https://pokeapi.co"
-                    baseUrl="/api"
-                    {...resourceList}
-                />
-            ))}
-
-            {docs.map(doc => (
-                <Fragment key={doc.name}>
-                    <h2 id={makeId(doc.name) + '-section'}>{doc.name}</h2>
-                    {doc.resources.map(resource => (
-                        <DocSection
-                            key={resource.name}
-                            rootUrl="https://pokeapi.co"
-                            baseUrl="/api"
-                            {...resource}
-                        />
-                    ))}
-                </Fragment>
-            ))}
-
-            <h2 id="utility-section">Utility</h2>
-            {utility.map(utilityResource => (
-                <DocSection
-                    key={utilityResource.name}
-                    rootUrl="https://pokeapi.co"
-                    baseUrl="/api"
-                    {...utilityResource}
-                />
-            ))}
-
-
+            {docs.map(
+                (doc, i) =>
+                    doc.separator ? (
+                        <hr key={i} />
+                    ) : (
+                        <Fragment key={doc.name}>
+                            <h2 id={makeId(doc.name) + '-section'}>
+                                {doc.name}
+                            </h2>
+                            {doc.description && (
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: marked(doc.description),
+                                    }}
+                                />
+                            )}
+                            {doc.resources.map(resource => (
+                                <DocSection
+                                    key={resource.name}
+                                    rootUrl="https://pokeapi.co"
+                                    baseUrl="/api"
+                                    {...resource}
+                                />
+                            ))}
+                        </Fragment>
+                    )
+            )}
         </DocsContainer>
     </Layout>
 );

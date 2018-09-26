d66c4b918f1be426b08053f62c2776b3e4ab64b2
Charles Marttinen <cmmartti@users.noreply.github.com>
2018-09-26 16:02:27 -0400

    Add a dismiss button to alerts (#8)
    * Add an unpin/pin button to alerts
    
    "Important" alerts have a sticky position by default. I don't like
    sticky/fixed elements that I can't dismiss because they're annoying,
    so this commit introduces an unpin/pin button on the alert that
    un-stickies it.
    
    * Change unpin button to a dismiss button
    
    Rather than showing an unpin button only on "important" alerts, which
    created a confusing UX, add a dismiss button to all alerts that
    permanently dismisses the alert.
    
    * fix: Store alert prefs in localStorage
    
    Also, hide alerts by default, and only show them if the user has not
    dismissed them. With this change, clients without JavaScript will not
    see any alerts.
    
    * fix: Change initial alert state to be dismissed

--- src/components/Alerts/index.js
+++ src/components/Alerts/index.js
@@ -1,32 +1,67 @@
 import React from 'react';
 import marked from 'marked';
 import classNames from 'classnames';
+import hashSum from 'hash-sum';
 
 import alerts from '../../../alerts.json';
 import styles from './index.module.scss';
+import XSvg from '../../images/ex.svg';
 
-const Alert = ({message, important}) => (
-    <div
-        className={classNames({
-            [styles.alert]: true,
-            [styles.important]: important,
-        })}
-    >
-        <div
-            className={styles.container}
-            dangerouslySetInnerHTML={{
-                __html: marked(message),
-            }}
-        />
-    </div>
-);
+class Alert extends React.Component {
+    state = {dismissed: true};
+    componentDidMount() {
+        this.setState({dismissed: !!localStorage.getItem(this.props.id)});
+    }
+    dismiss = () => {
+        localStorage.setItem(this.props.id, '1');
+        this.setState({dismissed: true});
+    };
+    render() {
+        if (this.state.dismissed) return null;
 
-export default () => {
+        return (
+            <div
+                className={classNames({
+                    [styles.alert]: true,
+                    [styles.level_important]: this.props.level === 'important',
+                    [styles.level_success]: this.props.level === 'success',
+                    [styles.level_info]: this.props.level === 'info',
+                })}
+            >
+                <div className={styles.container}>
+                    <div
+                        className={styles.message}
+                        dangerouslySetInnerHTML={{
+                            __html: this.props.htmlMessage,
+                        }}
+                    />
+                    <button
+                        title="Dismiss"
+                        className={styles.dismiss_button}
+                        onClick={this.dismiss}
+                    >
+                        <span hidden>Dismiss</span>
+                        <XSvg className={styles.image} aria-hidden />
+                    </button>
+                </div>
+            </div>
+        );
+    }
+}
+
+const Alerts = () => {
     const activeAlerts = alerts.filter(alert => alert.active !== false);
     if (activeAlerts.length > 0) {
         return activeAlerts.map(alert => (
-            <Alert key={alert.message} {...alert} />
+            <Alert
+                key={alert.message}
+                htmlMessage={marked(alert.message)}
+                level={alert.level || 'info'}
+                id={hashSum(alert)}
+            />
         ));
     }
     return;
 };
+
+export default Alerts;

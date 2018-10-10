55efbb36dc30a87444331abef05c354af59e2809
usagi.f@gmail.com <usagi.f@gmail.com>
2018-10-10 14:45:53 +0900

    Update render after mount

--- src/components/CodeBox/index.js
+++ src/components/CodeBox/index.js
@@ -5,22 +5,38 @@ import ReactJson from 'react-json-view';
 
 import styles from './index.module.scss';
 
-const CodeBox = ({children, small, language = 'json', title}) => (
-    <div className={styles.container}>
-        {title && (
-            <h4 className={styles.title}>{title}</h4>
-        )}
-        <SyntaxHighlighter
-            language={language}
-            className={classNames({
-                [styles.codebox]: true,
-                [styles.small]: small,
-            })}
-        >
-            {children}
-        </SyntaxHighlighter>
-        <ReactJson src={JSON.parse(children)} collapsed={1} />
-    </div>
-);
+class CodeBox extends React.Component {
+    state = { loaded: false };
+    componentDidMount() {
+        this.setState({ loaded: !this.state.loaded });
+    }
+    rawRender(children, language = 'json', small) {
+        return (
+            <SyntaxHighlighter
+                language={language}
+                className={classNames({
+                    [styles.codebox]: true,
+                    [styles.small]: small,
+                })}
+            >
+                {children}
+            </SyntaxHighlighter>
+        )
+    }
+    explorerRender(children) {
+        return <ReactJson src={JSON.parse(children)} collapsed={1} />
+    }
+    render() {
+        const { children, small, language, title } = this.props;
+        return (
+            <div className={styles.container}>
+                {title && (
+                    <h4 className={styles.title}>{title}</h4>
+                )}
+                {this.state.loaded ? this.explorerRender(children) : this.rawRender(children, language, small)}
+            </div>
+        )
+    }
+}
 
 export default CodeBox;

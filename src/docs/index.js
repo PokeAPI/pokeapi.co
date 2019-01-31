d4113604960830a967acf488b6794fe3cdaaf783
Charles Marttinen <mail@charlesmarttinen.ca>
2019-01-31 17:06:46 -0500

    fix: Correct documentation errors

--- src/docs/index.js
+++ src/docs/index.js
@@ -8,50 +8,21 @@ import locations from './locations.json';
 import machines from './machines.json';
 import moves from './moves.json';
 import pokemon from './pokemon.json';
+
 import resourceLists from './resource-lists.json';
 import utility from './utility.json';
 
 export default [
-    {
-        name: 'Berries',
-        resources: berries,
-    },
-    {
-        name: 'Contests',
-        resources: contests,
-    },
-    {
-        name: 'Encounters',
-        resources: encounters,
-    },
-    {
-        name: 'Evolution',
-        resources: evolution,
-    },
-    {
-        name: 'Games',
-        resources: games,
-    },
-    {
-        name: 'Items',
-        resources: items,
-    },
-    {
-        name: 'Locations',
-        resources: locations,
-    },
-    {
-        name: 'Machines',
-        resources: machines,
-    },
-    {
-        name: 'Moves',
-        resources: moves,
-    },
-    {
-        name: 'Pokemon',
-        resources: pokemon,
-    },
+    {name: 'Berries', resources: berries},
+    {name: 'Contests', resources: contests},
+    {name: 'Encounters', resources: encounters},
+    {name: 'Evolution', resources: evolution},
+    {name: 'Games', resources: games},
+    {name: 'Items', resources: items},
+    {name: 'Locations', resources: locations},
+    {name: 'Machines', resources: machines},
+    {name: 'Moves', resources: moves},
+    {name: 'Pokemon', resources: pokemon},
 ];
 
 export {resourceLists, utility};

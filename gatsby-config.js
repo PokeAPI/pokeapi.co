13f317c91dda45e087c0ab1bdd0bd96c7b04f5d6
Charles Marttinen <cmmartti@users.noreply.github.com>
2019-04-20 19:42:39 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,6 +29,5 @@ module.exports = {
                 icon: 'src/images/icon.png', // This path is relative to the root of the site.
             },
         },
-        // 'gatsby-plugin-offline',
     ],
 };

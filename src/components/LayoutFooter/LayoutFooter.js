996294f43c6fe23f7013d8b1666a572f8ff99a20
Paul Hallett <phalt@users.noreply.github.com>
2020-02-18 10:10:46 +1300

    Fix the spelling of my name

--- src/components/LayoutFooter/LayoutFooter.js
+++ src/components/LayoutFooter/LayoutFooter.js
@@ -14,7 +14,7 @@ const Footer = () => (
         <div className={styles.container}>
             <p>
                 © 2013–2019{' '}
-                <a href="https://phalt.github.io/">Paul Hallet</a> and{' '}
+                <a href="https://phalt.github.io/">Paul Hallett</a> and{' '}
                 <a href="https://github.com/PokeAPI/pokeapi#contributing">
                     PokéAPI contributors
                 </a>

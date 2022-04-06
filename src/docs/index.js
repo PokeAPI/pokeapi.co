1df46a737dba59cc9fb477d3fae27c90e2b1296f
Gabriel Sroka <gabrielsroka@gmail.com>
2022-04-06 16:11:19 -0700

    Update index.js
    Fix minor typo

--- src/docs/index.js
+++ src/docs/index.js
@@ -17,7 +17,7 @@ export default [
         name: 'Resource Lists/Pagination',
         resources: resourceLists,
         description:
-            "Calling any API endpoint without a resource ID or name will return a paginated list of available resources for that API. By default, a list \"page\" will contain up to 20 resources. If you would like to change this just add a 'limit' query parameter to the GET request, e.g. `?=60`. You can use 'offset' to move to the next page, e.g. `?limit=60&offset=60`.",
+            "Calling any API endpoint without a resource ID or name will return a paginated list of available resources for that API. By default, a list \"page\" will contain up to 20 resources. If you would like to change this just add a 'limit' query parameter to the GET request, e.g. `?limit=60`. You can use 'offset' to move to the next page, e.g. `?limit=60&offset=60`.",
         endOfSection: true,
     },
     {name: 'Berries', resources: berries},

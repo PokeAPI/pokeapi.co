42623e9845acebb4bfe3a9bb5802bda8a2a51e0a
Alessandro Pezzè <igougi.ui@gmail.com>
2024-02-03 23:13:06 +0100

    refactor: new analytics

--- src/App.js
+++ src/App.js
@@ -25,16 +25,13 @@ function App() {
         <Root>
             <Head>
                 <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9300109033485921" crossorigin="anonymous"></script>                
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-53299289-1"></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-X3X3FV0WED"></script>
                 <script>
-                {
-                    `window.dataLayer = window.dataLayer || [];
-                    function gtag() {
-                        dataLayer.push(arguments)
-                    }
-                    gtag('js', new Date());
-                    gtag('config', 'UA-53299289-1');`
-                }
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                
+                  gtag('config', 'G-X3X3FV0WED');
                 </script>
 
                 <html lang="en" />

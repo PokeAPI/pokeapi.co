1ec48e8406e72b12acea7d6d216631fa1244046c
indyandie <1058837+Indyandie@users.noreply.github.com>
2024-03-14 23:48:42 -0500

    feat: add title

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight()]
+  integrations: [starlight(
+    title: 'The RESTful Pokémon API',
+  )]
 });

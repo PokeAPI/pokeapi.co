bad102f573d57c3ecaa15d4ce270f05c16c6f097
Paul Hallett <phalt@users.noreply.github.com>
2024-09-20 12:11:18 +0000

    Update website and link to contributors
    Context:
    
    - My personal website is no longer this, it is just my Github page.
    - The contributors page has it's own link now on GitHub.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,7 @@ export default function Footer({id}) {
         <footer id={id} className={styles.footer}>
             <div className={styles.container}>
                 <div>
-                    Created by <a href="https://phalt.github.io/">Paul Hallett</a> and other <a href="https://github.com/PokeAPI/pokeapi#contributing">PokéAPI contributors</a> around the world. Pokémon and Pokémon character names are trademarks of Nintendo.
+                    Created by <a href="https://github.com/phalt">Paul Hallett</a> and other <a href="https://github.com/PokeAPI/pokeapi/graphs/contributors">PokéAPI contributors</a> around the world. Pokémon and Pokémon character names are trademarks of Nintendo.
                 </div>
                 <div>
                     <a href="https://pokeapi.statuspage.io/"><img alt="Status" src="https://img.shields.io/badge/dynamic/json?color=blue&label=status&query=%24.status.description&url=https%3A%2F%2Fzlfyqp3hlvly.statuspage.io%2Fapi%2Fv2%2Fsummary.json"></img></a>

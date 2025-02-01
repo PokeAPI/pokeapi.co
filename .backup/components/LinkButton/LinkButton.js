a4ae9d4d0632236b0a431df8d646e4f76cde1f69
indyandie <1058837+Indyandie@users.noreply.github.com>
2025-01-31 22:35:24 -0600

    chore: remove backups

--- .backup/components/LinkButton/LinkButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './LinkButton.module.scss';
-
-const LinkButton = ({className, ...props}) => (
-    <button className={styles.linkbutton + ' ' + className} {...props} />
-);
-
-export default LinkButton;

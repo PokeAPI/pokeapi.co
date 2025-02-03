45b8a1bbb29cb01625044db5e08ff2bc01f57cc3
Naramsim <igougi.ui@gmail.com>
2025-02-03 13:02:34 +0100

    feat: partial sub-root support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import starlight from '@astrojs/starlight'
 import starlightOpenAPI, { openAPISidebarGroups } from 'starlight-openapi'
 import tailwindcss from '@tailwindcss/vite'
 
+const base_url = process.env.BASE_URL || ''
+
 export default defineConfig({
   site: 'https://pokeapi.co',
+  base: base_url,
   integrations: [
     starlight({
       title: 'PokéAPI',
@@ -23,7 +26,7 @@ export default defineConfig({
         // Generate the OpenAPI documentation pages.
         starlightOpenAPI([
           {
-            base: '/v2/openapi',
+            base: `${base_url}/v2/openapi`,
             label: 'OpenAPI',
             schema: 'https://raw.githubusercontent.com/PokeAPI/pokeapi/master/openapi.yml',
           },

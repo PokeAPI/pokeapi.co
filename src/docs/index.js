f5b542024eeb8f8501e074c4f1333ae2a40a0915
Anh Thang Bui <buianhthang89@gmail.com>
2026-08-05 13:24:54 +0700

    add currency to menu

--- src/docs/index.js
+++ src/docs/index.js
@@ -1,5 +1,6 @@
 import berries from './berries.json';
 import contests from './contests.json';
+import currencies from './currencies.json';
 import encounters from './encounters.json';
 import evolution from './evolution.json';
 import games from './games.json';
@@ -20,15 +21,16 @@ export default [
             "Calling any API endpoint without a resource ID or name will return a paginated list of available resources for that API. By default, a list \"page\" will contain up to 20 resources. If you would like to change this just add a 'limit' query parameter to the GET request, e.g. `?limit=60`. You can use 'offset' to move to the next page, e.g. `?limit=60&offset=60`. `characteristic`, `contest-effect`, `evolution-chain`, `machine`, `super-contest-effect` endpoints are unnamed, the rest are named.",
         endOfSection: true,
     },
-    {name: 'Berries', resources: berries},
-    {name: 'Contests', resources: contests},
-    {name: 'Encounters', resources: encounters},
-    {name: 'Evolution', resources: evolution},
-    {name: 'Games', resources: games},
-    {name: 'Items', resources: items},
-    {name: 'Locations', resources: locations},
-    {name: 'Machines', resources: machines},
-    {name: 'Moves', resources: moves},
-    {name: 'Pokémon', resources: pokemon, endOfSection: true},
-    {name: 'Utility', resources: utility},
+    { name: 'Berries', resources: berries },
+    { name: 'Contests', resources: contests },
+    { name: 'Currencies', resources: currencies },
+    { name: 'Encounters', resources: encounters },
+    { name: 'Evolution', resources: evolution },
+    { name: 'Games', resources: games },
+    { name: 'Items', resources: items },
+    { name: 'Locations', resources: locations },
+    { name: 'Machines', resources: machines },
+    { name: 'Moves', resources: moves },
+    { name: 'Pokémon', resources: pokemon, endOfSection: true },
+    { name: 'Utility', resources: utility },
 ];
